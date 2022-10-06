c6bf707c991d4a97b2c4a3c64c99910d9afd7999
Kuprid3 <115141715+Kuprid3@users.noreply.github.com>
2022-10-06 15:29:52 +0700

    tugas1
    selesai

---
+++ src/latihan/satu/latihansatu.java
@@ -0,0 +1,31 @@
+package latihan.satu;
+
+import java.util.Scanner;
+
+public class latihansatu {
+	static String nama;
+	int usia;
+	char inisial;
+	public static String getNama(String inNama){
+		nama=inNama;
+		return nama;
+	}
+	public int getUsia(int inUsia){
+		 usia=inUsia;
+		 return usia;	
+	}
+	public char getinisial(char inInisial){
+		inisial=inInisial;
+		return inisial;
+	}
+	public static void showNama(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("nama :");
+		String valNama=input.nextLine();	
+		getNama(valNama);
+	}
+	public static void main (String[]args){
+		showNama();
+		System.out.println("Nama ini "+nama);
+	}
+}
